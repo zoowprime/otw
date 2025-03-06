f42c348fad009de65323bff7b6146af5107b519f
zoowprime <logantrilator@gmail.com>
2025-03-06 01:19:40 +0100

    Update anonymous.js

--- src/commands/anonymous.js
+++ src/commands/anonymous.js
@@ -13,7 +13,6 @@ module.exports = {
     ),
   async execute(interaction) {
     const anonMessage = interaction.options.getString('message');
-    // Récupérer le salon anonyme via la variable d'environnement
     const channelId = process.env.ANONYMOUS_CHANNEL_ID;
     const channel = await interaction.client.channels.fetch(channelId);
     if (!channel) {
@@ -22,9 +21,7 @@ module.exports = {
         ephemeral: true
       });
     }
-    // Envoyer le message anonyme dans le salon
     await channel.send(`💬 **Message anonyme :** ${anonMessage}`);
-    // Répondre en DM avec un embed
     await interaction.reply({
       embeds: [new EmbedBuilder().setColor(0xff0000).setDescription("✅ Votre message a été envoyé anonymement !")],
       ephemeral: true
