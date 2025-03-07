d8ba05569725cc5c72f30008fc574b0b793b21c9
zoowprime <logantrilator@gmail.com>
2025-03-08 00:32:43 +0100

    Update testData.js

--- src/testData.js
+++ src/testData.js
@@ -1,9 +1,11 @@
-// testData.js
+// src/testData.js
 const fs = require('fs');
 
+const dataPath = 'C:\\data'; // Notez les doubles antislashs pour Windows
+
 try {
-  const files = fs.readdirSync('/data');
-  console.log('Contenu de /data :', files);
+  const files = fs.readdirSync(dataPath);
+  console.log('Contenu de C:\\data :', files);
 } catch (err) {
-  console.error('Erreur en listant /data :', err);
+  console.error('Erreur en listant C:\\data :', err);
 }
