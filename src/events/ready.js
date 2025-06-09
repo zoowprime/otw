1ecb2fdb25e93f01097012d5283ca7c3f31cc440
zoowprime <logantrilator@gmail.com>
2025-06-09 23:04:59 +0200

    Delete src/events/ready.js

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  async execute(client) {
-    console.log(`✅ Connecté en tant que ${client.user.tag}`);
-    const alimentationSystem = require('./alimentationSystem');
-    alimentationSystem.start(client);
-  }
-};
