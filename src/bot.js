d9407a1ccedde6cda858e4fdc358a48d880d4a97
zoowprime <logantrilator@gmail.com>
2025-06-09 17:54:24 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -120,27 +120,6 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-// Gestion commandes session lancee
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isButton()) return;
-  const role = interaction.guild.roles.cache.get('1378037596566978561');
-  const member = interaction.member;
-  if (!role || !member) return;
-  try {
-    if (interaction.customId === 'en_ville') {
-      await member.roles.add(role);
-      await interaction.reply({ content: ✅ ${member} est maintenant marqué comme en ville., ephemeral: true });
-    } else if (interaction.customId === 'deconnecte') {
-      await member.roles.remove(role);
-      await interaction.reply({ content: ❌ ${member} a été marqué comme déconnecté., ephemeral: true });
-    }
-  } catch (err) {
-    console.error('Erreur lors de l’attribution du rôle :', err);
-    await interaction.reply({ content: '❗ Une erreur est survenue.', ephemeral: true });
-    }
-  }
-});
-
 // Gestion des commandes texte pour l'économie (préf. "!")
 client.on('messageCreate', async (message) => {
   if (message.author.bot || !message.guild) return;
