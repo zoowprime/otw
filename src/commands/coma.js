a0f5dfadddc91fc15dda1c0d04f62a90621b2c1f
zoowprime <logantrilator@gmail.com>
2025-07-05 21:55:37 +0200

    Create coma.js

---
+++ src/commands/coma.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('coma')
+    .setDescription('💤 Plonge un joueur dans un coma RP')
+    .addUserOption(option =>
+      option
+        .setName('cible')
+        .setDescription('Le joueur à mettre en coma')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('durée')
+        .setDescription('Durée du coma (ex: 30m, 2h)')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('raison')
+        .setDescription('Raison du coma')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const cible  = interaction.options.getUser('cible');
+    const duree  = interaction.options.getString('durée');
+    const raison = interaction.options.getString('raison');
+
+    const embed = new EmbedBuilder()
+      .setColor(0xFFFFFF)                         // blanc
+      .setTitle('💤 État de coma déclenché')
+      .setDescription(`${cible} est maintenant en coma RP… Zzz`)
+      .addFields(
+        { name: '🎯 Joueur concerné', value: `${cible}`, inline: true },
+        { name: '⏱️ Durée du coma',    value: duree,      inline: true },
+        { name: '❓ Raison du coma',    value: raison,     inline: false },
+      )
+      .setFooter({ text: 'Ils se réveilleront quand leur temps sera écoulé !' });
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
