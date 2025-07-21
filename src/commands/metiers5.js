28f9e9a15a5227103ec7b98732214c40af7d3e9d
zoowprime <logantrilator@gmail.com>
2025-07-21 23:18:01 +0200

    Update metiers5.js

--- src/commands/metiers5.js
+++ src/commands/metiers5.js
@@ -31,30 +31,6 @@ module.exports = {
           '**Paiement :** à chaque livraison/mission\n' +
           '**Salaire :** 20 à 40 $ / mission\n' +
           'Peut négocier avec le gouvernement ou des entreprises.\n\n' +
-
-          '**_Agent de transport_**\n' +
-          '**Rémunération :** par le directeur\n' +
-          '**Paiement :** par livraison\n' +
-          '**Salaire :** 10 à 15 $ / session\n' +
-          'Danger potentiel selon les trajets.\n\n' +
-
-          '**_Patron d’écurie_**\n' +
-          '**Rémunération :** par les ventes / locations de chevaux\n' +
-          '**Paiement :** variable, selon fréquentation\n' +
-          '**Salaire :** 20 à 35 $ / session\n' +
-          'S’il y a du passage, c’est rentable.\n\n' +
-
-          '**_Écuyer / Soigneur_**\n' +
-          '**Rémunération :** par le patron\n' +
-          '**Paiement :** par session travaillée\n' +
-          '**Salaire :** 6 à 10 $ / session\n' +
-          'Moins payé, mais très présent RP.\n\n' +
-
-          '**_Contremaître_**\n' +
-          '**Rémunération :** par les mines/entreprises\n' +
-          '**Paiement :** par journée d’encadrement\n' +
-          '**Salaire :** 12 à 18 $ / session\n' +
-          'Gère les ouvriers, parfois impliqué dans les conflits sociaux.'
         });
 
     await interaction.reply({ embeds: [embed] });
