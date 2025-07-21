9377958595f8dba77c0dd770ae7194ea72a9872d
zoowprime <logantrilator@gmail.com>
2025-07-21 22:54:16 +0200

    Update metiers5.js

--- src/commands/metiers5.js
+++ src/commands/metiers5.js
@@ -1,1 +1,62 @@
+// src/commands/metiers3.js
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('metiers5')
+    .setDescription('Affiche la liste des métiers RP disponibles avec leurs rémunérations.'),
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle('📜 Liste des métiers')
+      .setColor(0xFFFFFF)
+
+// 🏘️ Institutions et économie
+      .addFields({
+        name: '🏘️ **INSTITUTIONS ET ÉCONOMIE²**',
+        value:
+          '**_Patron armurerie_**\n' +
+          '**Rémunération :** par les ventes\n' +
+          '**Paiement :** quotidienne selon flux\n' +
+          '**Salaire :** 15 à 30 $ / session\n' +
+          'Dépend fortement de la demande en armes.\n\n' +
+
+          '**_Armurier_**\n' +
+          '**Rémunération :** par le patron\n' +
+          '**Paiement :** par session travaillée\n' +
+          '**Salaire :** 8 à 12 $ / session\n' +
+          'Main d’œuvre qualifiée.\n\n' +
+
+          '**_Patron d’agence de transport_**\n' +
+          '**Rémunération :** par contrat (état, privé)\n' +
+          '**Paiement :** à chaque livraison/mission\n' +
+          '**Salaire :** 20 à 40 $ / mission\n' +
+          'Peut négocier avec le gouvernement ou des entreprises.\n\n' +
+
+          '**_Agent de transport_**\n' +
+          '**Rémunération :** par le directeur\n' +
+          '**Paiement :** par livraison\n' +
+          '**Salaire :** 10 à 15 $ / session\n' +
+          'Danger potentiel selon les trajets.\n\n' +
+
+          '**_Patron d’écurie_**\n' +
+          '**Rémunération :** par les ventes / locations de chevaux\n' +
+          '**Paiement :** variable, selon fréquentation\n' +
+          '**Salaire :** 20 à 35 $ / session\n' +
+          'S’il y a du passage, c’est rentable.\n\n' +
+
+          '**_Écuyer / Soigneur_**\n' +
+          '**Rémunération :** par le patron\n' +
+          '**Paiement :** par session travaillée\n' +
+          '**Salaire :** 6 à 10 $ / session\n' +
+          'Moins payé, mais très présent RP.\n\n' +
+
+          '**_Contremaître_**\n' +
+          '**Rémunération :** par les mines/entreprises\n' +
+          '**Paiement :** par journée d’encadrement\n' +
+          '**Salaire :** 12 à 18 $ / session\n' +
+          'Gère les ouvriers, parfois impliqué dans les conflits sociaux.'
+        });
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
