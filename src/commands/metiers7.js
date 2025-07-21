fa6bfd1b2556934637e8feb95adca76a5782df72
zoowprime <logantrilator@gmail.com>
2025-07-21 23:10:18 +0200

    Update metiers7.js

--- src/commands/metiers7.js
+++ src/commands/metiers7.js
@@ -30,31 +30,7 @@ module.exports = {
           '**Rémunération :** par les patients ou la ville\n' +
           '**Paiement :** à l’acte\n' +
           '**Salaire :** 10 à 20 $ / session\n' +
-          'Peut gagner plus selon la réputation.\n\n' +
-
-          '**_Directeur de cabinet de journalisme_**\n' +
-          '**Rémunération :** selon ventes ou subventions\n' +
-          '**Paiement :** hebdo ou à la publication\n' +
-          '**Salaire :** 15 à 25 $ / session\n' +
-          'Peut être influent s’il crée l’actualité.\n\n' +
-
-          '**_Journaliste_**\n' +
-          '**Rémunération :** par article publié\n' +
-          '**Paiement :** à chaque reportage\n' +
-          '**Salaire :** 5 à 15 $ / article\n' +
-          'Très RP, dépend du rythme d’écriture.\n\n' +
-
-          '**_Patron de distillerie_**\n' +
-          '**Rémunération :** par les ventes d’alcool\n' +
-          '**Paiement :** par session active\n' +
-          '**Salaire :** 20 à 35 $ / session\n' +
-          'Illégal ou non, grosse marge possible.\n\n' +
-
-          '**_Distillateur_**\n' +
-          '**Rémunération :** par le patron\n' +
-          '**Paiement :** à la production\n' +
-          '**Salaire :** 8 à 12 $ / session\n' +
-          'Peut aussi participer à des missions de contrebande.'
+          'Peut gagner plus selon la réputation.'
         });
 
     await interaction.reply({ embeds: [embed] });
