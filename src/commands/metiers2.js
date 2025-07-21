ee6e707ae7a73bb41ed384f788bfa96582616cc8
zoowprime <logantrilator@gmail.com>
2025-07-21 22:40:03 +0200

    Update metiers2.js

--- src/commands/metiers2.js
+++ src/commands/metiers2.js
@@ -49,30 +49,6 @@ module.exports = {
           '**Paiement :** selon implication\n' +
           '**Salaire :** 8 à 12 $ / session\n' +
           'Statut plus flexible, souvent temporaire.\n\n' +
-
-          '**_Directeur BFRS_**\n' +
-          '**Rémunération :** par l’État\n' +
-          '**Paiement :** à chaque grande opération\n' +
-          '**Salaire :** 35 à 40 $ / session\n' +
-          'Hautement stratégique, très bien payé.\n\n' +
-
-          '**_BFRS – Section Alpha (Renseignement)_**\n' +
-          '**Rémunération :** par l’État\n' +
-          '**Paiement :** à chaque mission/filature\n' +
-          '**Salaire :** 20 à 25 $ / session\n' +
-          'Agents discrets, parfois infiltrés. Risque élevé, paiement secret.\n\n' +
-
-          '**_Milice nationale_**\n' +
-          '**Rémunération :** par l’État (ou gouverneur selon contexte)\n' +
-          '**Paiement :** à chaque mobilisation\n' +
-          '**Salaire :** 10 à 15 $ / session\n' +
-          'Moins stable, souvent mobilisé lors de crises.\n\n' +
-
-          '**_Garde du corps_**\n' +
-          '**Rémunération :** privée\n' +
-          '**Paiement :** selon le contrat ou jour de service\n' +
-          '**Salaire :** 10 à 20 $ / session\n' +
-          'Peut varier selon la notoriété du client.'
          });
 
     await interaction.reply({ embeds: [embed] });
