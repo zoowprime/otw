e3b1d736a0ae552b4b1c96d19b92831837ddfb6c
zoowprime <logantrilator@gmail.com>
2025-07-27 02:15:37 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -65,6 +65,7 @@ require('./events/faimSoifSystem')(client);
 require('./events/raidProtect')(client);
 require('./events/qcmNoCmd')(client);
 require('./events/heistSessions')(client);
+require('./events/candidature')(client);
 
 // Chargement des commandes slash
 client.commands = new Collection();
