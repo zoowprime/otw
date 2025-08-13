d588bcf7733b8128eecf48d6396e060d1a84890b
zow.py <logantrilator@gmail.com>
2025-08-13 23:12:56 +0200

    Create weaponsCatalog.js

---
+++ src/data/weaponsCatalog.js
@@ -0,0 +1,49 @@
+module.exports = {
+  categories: [
+    {
+      name: 'Pistolet/Revolver 🔫',
+      items: [
+        'Revolver Cattleman',
+        'Revolver double action',
+        'Revolver Schofield',
+        'Revolver navy',
+        'Revolver LeMat',
+        'Pistolet volcanic',
+      ],
+    },
+    {
+      name: 'fusils et carabines 🎯',
+      items: [
+        'Carabine à répétition',
+        'Carabine Lancaster',
+        'Carabine Evans Rifle',
+        'Carabine Litchfield Rifle',
+        'Fusil Springfield',
+        'Fusil à verrou',
+        'Fusil à petit gibier',
+      ],
+    },
+    {
+      name: 'Armes de distance 🏹',
+      items: [
+        'Arc',
+        'Arc amélioré',
+        'Couteau de lancer',
+        'Lasso',
+      ],
+    },
+    {
+      name: 'Arme blanche 🪓',
+      items: [
+        'Couteau',
+        'Couteau de chasse',
+        'Couteau en os',
+        'Cisaille',
+        'Hache',
+        'Hachette',
+        'Machette',
+        'Marteau',
+      ],
+    },
+  ],
+};
