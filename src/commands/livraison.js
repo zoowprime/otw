a421a7815f1080d868c9d5c5baa96b4709e40bcf
zow.py <logantrilator@gmail.com>
2025-08-17 22:25:36 +0200

    Update livraison.js

--- src/commands/livraison.js
+++ src/commands/livraison.js
@@ -1,26 +1,41 @@
+// src/commands/livraison.js
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { getSummaryLines } = require('../interaction/trainStock');
+
+const GARES = [
+  'Gare de Rhodes',
+  'Gare de Valentine',
+  'Gare de Saint-Denis',
+  'Docks de Saint-Denis',
+  'Saloon de Valentine',
+  'Saloon de Rhodes',
+  'Emerald Ranch'
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('livraison')
-    .setDescription('Permet aux transporteurs de livrer des ressources entre les villes.')
-    .addStringOption(option =>
-      option.setName('produit')
-        .setDescription('Le produit à livrer')
+    .setDescription('Annoncer une livraison du train vers une gare')
+    .addStringOption(o =>
+      o.setName('gare')
+        .setDescription('Gare de livraison')
         .setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('destination')
-        .setDescription('La destination de la livraison')
-        .setRequired(true)
+        .addChoices(...GARES.map(g => ({ name: g, value: g })))
     ),
+
   async execute(interaction) {
-    const produit = interaction.options.getString('produit');
-    const destination = interaction.options.getString('destination');
+    const gare = interaction.options.getString('gare', true);
+    const { chev, arm } = getSummaryLines();
     const embed = new EmbedBuilder()
-      .setColor(0xCC9900) // Jaune foncé
-      .setTitle("Livraison en cours")
-      .setDescription(`Vous effectuez une livraison de **${produit}** vers **${destination}**. Dépêchez-vous de livrer les ressources le plus vite possible !`);
+      .setColor(0x3498db)
+      .setTitle('🚂 Livraison en cours')
+      .setDescription(`**${interaction.user}** effectue une livraison à **${gare}**.`)
+      .addFields(
+        { name: '🐎 Chevaux', value: chev, inline: false },
+        { name: '🔫 Armes',   value: arm,  inline: false }
+      )
+      .setTimestamp();
+
     await interaction.reply({ embeds: [embed] });
-  },
+  }
 };
