2235dd592f4166b7e0f414f099af412eddbd3289
zow.py <logantrilator@gmail.com>
2025-11-06 23:40:06 +0100

    Rename weaponsCatalog.js to catalogWeapons.js

---
+++ src/data/catalogWeapons.js
@@ -0,0 +1,31 @@
+// src/data/catalogWeapons.js
+// Prix d'import (référence tes listes)
+const WEAPONS = [
+  { name: 'Cattleman Revolver', importPrice: 18.5 },
+  { name: 'Navy Revolver', importPrice: 18.0 },
+  { name: 'Double Action Revolver', importPrice: 19.0 },
+  { name: 'Schofield Revolver', importPrice: 20.5 },
+  { name: 'Lemat Revolver', importPrice: 25.25 },
+  { name: 'Volcanic Pistol', importPrice: 18.5 },
+  { name: 'Litchfield Rifle', importPrice: 26.25 },
+  { name: 'Evans Rifle', importPrice: 32.25 },
+  { name: 'Lancaster Rifle', importPrice: 32.25 },
+  { name: 'Carabine à Répétition', importPrice: 32.25 },
+  { name: 'Fusil à Petit Gibier', importPrice: 15.25 },
+  { name: 'Fusil Springfield', importPrice: 19.75 },
+  { name: 'Fusil à Verrou', importPrice: 26.25 },
+  { name: 'Couteau de Lancer', importPrice: 2.5 },
+  { name: 'Lasso', importPrice: 1.5 },
+  { name: 'Arc', importPrice: 4.5 },
+  { name: 'Arc Amélioré', importPrice: 7.5 },
+  { name: 'Couteau', importPrice: 3.0 },
+  { name: 'Cisaille', importPrice: 2.5 },
+  { name: 'Couteau de Chasse', importPrice: 4.5 },
+  { name: 'Couteau en Os', importPrice: 3.5 },
+  { name: 'Marteau', importPrice: 3.0 },
+  { name: 'Hachette', importPrice: 4.0 },
+  { name: 'Hache', importPrice: 5.0 },
+  { name: 'Machette', importPrice: 4.5 },
+];
+
+module.exports = { WEAPONS };
