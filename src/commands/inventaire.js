0f11f0a6177cfe03a911abe9880c17fb060f7acd
zow.py <logantrilator@gmail.com>
2025-11-12 02:39:20 +0100

    Update inventaire.js

--- src/commands/inventaire.js
+++ src/commands/inventaire.js
@@ -55,7 +55,7 @@ const GRID = {
 
 // Micro-ajustements par colonne/ligne.
 // Ajuste ici si tu veux re-centrer précisément des colonnes/lignes entières :
-const COL_NUDGE = [ 0, 0, 8, 23, 35 ];   // <- j’ai avancé un peu les colonnes 3,4,5 vers la DROITE
+const COL_NUDGE = [ 0, 0, 11, 40, 60 ];   // <- j’ai avancé un peu les colonnes 3,4,5 vers la DROITE
 const ROW_NUDGE = [ 0, 0, 0, 0, 0 ];
 
 // Position du poids ACTUEL (le “/ 60.00” est déjà sur l’image)
